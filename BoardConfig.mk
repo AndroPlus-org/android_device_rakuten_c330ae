82656926376dc83f51be76165b57e046e04f4630
AndroPlus <mail@andro.plus>
2020-06-27 12:17:41 +0900

    Set flag to copy fingerprint and product from installed system

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -123,3 +123,4 @@ TW_NO_EXFAT_FUSE := false
 TW_INCLUDE_NTFS_3G := true
 TW_HAS_EDL_MODE := true
 TW_INCLUDE_RESETPROP := true
+TW_OVERRIDE_SYSTEM_PROPS := "ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.sdk;ro.build.version.security_patch;ro.build.version.release"
